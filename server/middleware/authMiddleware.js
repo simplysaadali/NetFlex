c7a98617a9ea3986541f2b6e8f5746551d7cca8d
simplysaadali <ali091g@gmail.com>
2026-09-13 02:14:06 +0500

    Middleware revision and name replacement for better understandings and gen token separate file

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+function protect (req, res, next){
+    const token = req.cookies.token;
+
+    if(!token)
+        return res.status(401).json({
+            message: "Unauthorized - No Token"
+        });
+    
+    try {
+        const decrypted = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = {
+            id: decrypted.id,
+        };
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Token invalid or expired" });
+    }
+}
+
+export default protect;
