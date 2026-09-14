0abe0ee7ee4c3a740baeb9a6a7bb580bd19b7f07
simplysaadali <ali091g@gmail.com>
2026-09-14 19:09:11 +0500

    solved a small error in the backend to run the code

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, getProfile, getUser, getUsers, updateProfile, updateUser } from "../controller/userController.js";
-import protect from '../middleware/authMiddleware';
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
