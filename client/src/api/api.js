215f8456cd275a58dd30402375a55962d25a2c1b
simplysaadali <ali091g@gmail.com>
2026-09-16 18:23:32 +0500

    some error solving

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-});
-
-// Attach the auth token (if present) to every request
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  withCredentials: true, // Required to send cookies (JWT) cross-origin
 });
 
 export default api;
